93759a6be6fa62d652b0ab71c584d8636e66b527
Gaspar <gmusitelli@edenschool.fr>
2022-03-01 16:43:21 +0100

    Correction du APIbase

--- client/src/utils/APIBase.js
+++ client/src/utils/APIBase.js
@@ -1,6 +1,5 @@
-import isDev from "electron-is-dev";
-
 let arr = ["http://localhost:4060", "https://api.synko.kunah.fr", "ws://localhost:7758", "wss://ws.synko.kunah.fr"];
+let { isDev } = require('../package.json');
 let WS_URL = isDev ? arr[2] : arr[3];
 let API_URL = isDev ? arr[0] : arr[1];
 export {
